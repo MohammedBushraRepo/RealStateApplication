87756c62b86ca08d7780540661ebe47664201ca8
MohammedBushraRepo <m.bushra9577@gmail.com>
2024-07-11 16:13:23 +0400

    full stack project

---
+++ client/src/firebase.js
@@ -0,0 +1,38 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "real-estate-eeea6.firebaseapp.com",
+  projectId: "real-estate-eeea6",
+  storageBucket: "real-estate-eeea6.appspot.com",
+  messagingSenderId: "677669847583",
+  appId: "1:677669847583:web:8e0bea5f4e6c4975378b2a"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+
+
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from 'firebase/app';
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: 'mern-estate.firebaseapp.com',
+//   projectId: 'mern-estate',
+//   storageBucket: 'mern-estate.appspot.com',
+//   messagingSenderId: '1078482850952',
+//   appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
---
+++ client/src/pages/About.jsx
@@ -0,0 +1,36 @@
+
+export default function About() {
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Housing Development & Construction Corporation</h1>
+      <p className='mb-4 text-slate-700'>Housing Development & Construction Corporation for
+General Contracting is introducing a new and impressive
+addition to the world of construction and building. The
+corporation stands out for its diverse and comprehensive
+services, which work to meet market needs comprehensively.
+Housing Development & Construction Corporation specializes in contracting and adopts an innovative approach to
+design and implementation that aligns with the latest
+developments in the construction industry. With a
+qualified and specialized team, the corporation excels in
+high-quality construction and finishing works, making it
+the preferred choice for large and small projects.
+Through a meticulous real estate strategy, the corporation
+provides various properties that meet customers diverse
+requirements. Whether you are looking for your dream
+home or an office for your business, the corporation
+offers customized solutions to meet your aspirations.
+Whether through marketing to sell real estate products or
+searching for real estate products that meet clients
+requirements and help them explore real estate investment opportunities.
+With technological development, the corporation aims to
+provide efficient electronic and governmental services
+that facilitate buying, selling, and marketing operations.
+ And finally, through the use of the latest technologies, the
+corporation delivers a seamless and convenient customer.</p>
+      <p className='mb-4 text-slate-700'>
+      Our mission is to help our clients achieve their real estate goals by providing expert advice, personalized service, and a deep understanding of the local market. Whether you are looking to buy, sell, or rent a property, we are here to help you every step of the way.
+      </p>
+      <p className='mb-4 text-slate-700'>Our team of agents has a wealth of experience and knowledge in the real estate industry, and we are committed to providing the highest level of service to our clients. We believe that buying or selling a property should be an exciting and rewarding experience, and we are dedicated to making that a reality for each and every one of our clients.</p>
+    </div>
+  )
+}
